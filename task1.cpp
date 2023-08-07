dbb87478832915c201b3afa59fa85d4fae78a9fb
simran-1118 <simrangarcha191@gmail.com>
2023-08-07 14:34:50 +0530

    Initial commit

---
+++ task1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+
+    srand(time(0));
+
+    int randomNumber = rand() % 100 + 1;
+
+  
+    int guess;
+    int attempts = 0;
+
+    cout<<"Welcome to the Number Guessing Game! "<<endl;
+    cout<<endl;
+
+    do{
+        cout<<"Enter Your guess (between 1 and 100): ";
+        cout<<endl;
+        cin>>guess;
+
+        if(guess > randomNumber){
+            cout<<"Too high! Try again: "<<endl;
+        }
+        else if(guess < randomNumber){
+            cout<<"Too low! Try again: "<<endl;
+        }
+
+        attempts++;
+        
+
+    }
+    while(guess != randomNumber);
+
+
+    cout<<"Congratulations! You guessed the number in "<<attempts<<" attempts:"<<endl;
+    cout<<endl;
+
+
+    return 0;
+}
