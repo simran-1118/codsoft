5121b262e6f8943eca9445de98f4a74e2b8c626b
simran-1118 <simrangarcha191@gmail.com>
2023-08-09 14:45:24 +0530

    Word Count

--- task3.cpp
+++ task3.cpp
@@ -6,37 +6,36 @@
 int main() {
     std::string filename;
     
-    // Prompt the user to enter the file name
+    
     std::cout << "Enter the name of the text file: ";
     std::cin >> filename;
     
-    // Open the file
+   
     std::ifstream inputFile(filename);
     
     if (!inputFile.is_open()) {
         std::cout << "Error opening the file." << std::endl;
-        return 1; // Return an error code
+        return 1; 
     }
     
     std::string line;
     int wordCount = 0;
     
-    // Read the file line by line
+    
     while (std::getline(inputFile, line)) {
         std::istringstream iss(line);
         std::string word;
         
-        // Count words in the line
+        
         while (iss >> word) {
-            wordCount++;
+            wordCount++;    
         }
     }
     
-    // Close the file
+    
     inputFile.close();
     
-    // Display the total word count
+
     std::cout << "Total word count: " << wordCount << std::endl;
     
-    return 0; // Return success code
-}
+    return 0; }
